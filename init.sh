c4be7aa202d1c32d72960c314be945ae99801014
garywei944 <33930674+garywei944@users.noreply.github.com>
2019-10-10 23:12:19 -0400

    v1.3.0
    * Stop using OSX specific configuration

--- init.sh
+++ init.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
 # macOS specific settings
-mkdir -p ../Default
-ln ../User/Preferences\ \(OSX\).sublime-settings ../Default/Preferences\ \(OSX\).sublime-settings
+# mkdir -p ../Default
+# ln ../User/Preferences\ \(OSX\).sublime-settings ../Default/Preferences\ \(OSX\).sublime-settings
 
 # Install Dependency packages
-pip install pip -U
 pip install CodeIntel
 npm install -g npm jshint csslint xg-htmlhint
